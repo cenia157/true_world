497e9f896cf56ad60672c89e4e8c300724b7fec5
cenia157 <allelouia1574@gmail.com>
2026-04-09 23:01:39 +0900

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class CameraFollow : MonoBehaviour 
+{ 
+    public Transform target; 
+    public Vector3 offset = new Vector3(0, 6, -6); 
+    void LateUpdate() { 
+        transform.position = target.position + offset; 
+    } 
+}
